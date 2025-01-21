65a9930dbe5281fb16745884082bf0917a309266
saravanaecom <saravanaecomweb@gmail.com>
2025-01-21 10:58:33 +0530

    Update AppLogo.jsx

--- src/components/logo/AppLogo.jsx
+++ src/components/logo/AppLogo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from './srlogo.webp';
+import Logo from './logo.png';
 const AppLogo = ({ children }) => {
   return (
     <img src={Logo} alt="logo" style={{width: '130px', height: '50px'}}/>
